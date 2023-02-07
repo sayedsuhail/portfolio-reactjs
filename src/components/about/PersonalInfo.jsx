d20d2ebb561d8eefb2c8bcf942b44e705c7b2ef8
Sayed Suhail <sayedsuhail006@gmail.com>
2023-02-07 20:40:28 +0500

    info updated

--- src/components/about/PersonalInfo.jsx
+++ src/components/about/PersonalInfo.jsx
@@ -3,13 +3,10 @@ import React from "react"
 const personalInfoContent = [
   { meta: "first name", metaInfo: "Sayed" },
   { meta: "last name", metaInfo: "Suhail" },
-  // { meta: "Age", metaInfo: "27 Years" },
-  // { meta: "Nationality", metaInfo: "Pakistani" },
   { meta: "Freelance", metaInfo: "Available" },
-  // { meta: "Address", metaInfo: "Islamabad, Pakistan" },
   { meta: "phone", metaInfo: "+92-3038813846" },
+  { meta: "Hours / Week", metaInfo: "30+" },
   { meta: "Email", metaInfo: "sayed@martzon.com" },
-  // { meta: "Skype", metaInfo: " steve.milner" },
   { meta: "langages", metaInfo: "Urdu, English" },
 ]
 
